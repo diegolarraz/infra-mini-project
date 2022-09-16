1634781798e671437ae049b8403215c0d3f71d48
Diego Larraz <diegolarraz1@gmail.com>
2022-09-16 14:06:58 +0100

    checkpoint - Event catchup when server is down

--- client/src/CommentList.jsx
+++ client/src/CommentList.jsx
@@ -1,25 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
 export default ({ comments }) => {
 
-    // const getComments = async () => {
-    //     const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
-    //     setComments(res.data)
-    // }
-
-    // useEffect(() => {
-    //     getComments()
-    // }, []);
-
     const renderedComments = comments.map(comment => {
+        let content
+
+        if (comment.status === 'approved') {
+            content = comment.content
+        }
+        if (comment.status === 'pending') {
+            content = 'This comment is awaiting moderation'
+        }
+        if (comment.status === 'rejected') {
+            content = 'this comment has been rejected'
+        }
         return (
-            <li key={comment.id}>{comment.content}</li>
+            <li key={comment.id}>{content}</li>
         )
     })
 
     return (
-        <div className="">
+        <div>
             {renderedComments}
         </div>
     )
